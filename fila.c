d919561e29fa61921846f09dc2700a2eab3a8bf7
Karina Tronkos <karina.tronkos@gmail.com>
2017-10-20 17:38:58 -0200

    Update fila.c

--- fila.c
+++ fila.c
@@ -5,16 +5,18 @@
 #define N 100
 
 struct fila {
-	int n;
-	int ini; /* marca a posicao do prox elemento a ser retirado da fila */
-	float vet[N];
+	int n; /* numero de elementos armazenados na fila */
+	int ini; /* indice para o inicio da fila */
+	InfoProcesso vet[N];
+	int prio;
 }
 
 /* Essa funcao para criar a fila aloca dinamicamente essa estrutura e inicializa a fila como sendo vazia. */
-Fila* fila_cria(void) {
+Fila* fila_cria(int prioridade) {
 	Fila* f = (Fila*) malloc(sizeof(Fila));
 	f->n = 0; /* inicializa fila vazia */
 	f->ini = 0; /* escolhe uma posicao inicial */
+	f->prio = prioridade;
 	return f;
 }
 
@@ -24,30 +26,30 @@ int fila_vazia(Fila *f) {
 }
 
 /* Para inserir um elemento na fila, usamos a prox posicao livre do vetor, indicada por fim. */
-void fila_insere(Fila* f, float v) {
+void fila_insere(Fila* f, InfoProcesso processo) {
 	int fim;
-	if (f->n == N) { */ fila cheia: capacidade esgotada */
+	if (f->n == N) { /* fila cheia: capacidade esgotada */
 	printf("Capacidade da fila estourou. \n")
 	exit(1);
 	}
 	/* insere elemento na prox posicao livre */
 	fim = (f->ini + f->n) % N;
-	f->vet[fim] = v;
+	f->vet[fim] = processo;
 	f->n++;
 }
 
 /* A funcao para retirar o elemento do inicio da fila fornece o valor do elemento retirado como retorno. */
-float fila_retira(Fila* f) {
-	float v;
+InfoProcesso fila_retira(Fila* f) {
+	InfoProcesso processo;
 	if (fila_vazia(f)) {
 	printf("Fila vazia. \n")
 	exit(1);
 	}
 	/* retira elemento do inicio */
-	v = f->vet[f->ini];
+	processo = f->vet[f->ini];
 	f->ini = (f->ini + 1) % N;
 	f->n--;
-	return v;
+	return processo;
 }
 
 /* Funcao para liberar a memoria alocada pela fila. */
