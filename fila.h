ee380e62091a00cbf503d598b0fff2e40cd29b1c
Karina Tronkos <karina.tronkos@gmail.com>
2017-10-20 17:38:37 -0200

    Update fila.h

--- fila.h
+++ fila.h
@@ -1,11 +1,15 @@
 typedef struct fila Fila;
 
-Fila* fila_cria(void);
+Fila* fila_cria(int prioridade);
 
-void fila_insere(Fila* f, float v);
+void fila_insere(Fila* f, InfoProcesso processo);
 
-float fila_retira(Fila* f);
+InfoProcesso fila_retira(Fila* f);
 
 int fila_vazia(Fila* f);
 
 void fila_libera(Fila *f);
+
+
+
+typedef struct infoProcesso InfoProcesso;
